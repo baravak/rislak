7a7569c5d4a23444c568dc6ef1f8ce9251b7fbde
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-07-13 15:55:46 +0430

    Minor fix

--- resources/views/dashboard/home/newMyRooms.blade.php
+++ resources/views/dashboard/home/newMyRooms.blade.php
@@ -1,7 +1,7 @@
 <h3 class="text-gray-700 variable-font-bold mb-4 mt-8">@lang('My therapy rooms')</h3>
 <div class="grid grid-cols-1 sm:grid-cols-2 2xl:grid-cols-4 gap-4 mt-4">
     @foreach ($user->rooms->where('acceptation.position', 'manager') as $room)
-        <div class="p-4 pl-12 pb-2 relative border border-gray-500 rounded">
+        <div class="p-4 pl-12 relative border border-gray-500 rounded">
             <a href="{{ route('dashboard.rooms.show', $room->id)  }}" class="flex items-center group">
                 <div class="flex justify-center items-center flex-shrink-0 w-16 h-16 bg-gray-300 text-gray-600 text-xs rounded-full border-2 border-white overflow-hidden">
                     @avatarOrName($room->center->detail)
