bda5d81eff7c86f3b5a3442739782d803c00a984
abouhatef <itb.baravak@gmial.com>
2021-07-15 11:34:10 +0430

    personal clinic tags

--- resources/views/dashboard/centers/showButtons.blade.php
+++ resources/views/dashboard/centers/showButtons.blade.php
@@ -30,10 +30,25 @@
                         <i class="w-6 text-center fal fa-edit pb-1"></i>
                         <span class="vaiable-font-medium mr-2">@lang('Edit')</span>
                     </a>
-                    <a href="{{ route('dashboard.center.setting.session-platforms', $center->id) }}" class="flex items-center text-sm text-gray-700 py-3 px-4 hover:bg-gray-100 border-b border-gray-100 transition dir-rtl">
+                    @if ($center->type == 'personal_clinic')
+                        <div class="flex items-center text-sm text-gray-800 py-3 px-4 bg-gray-100 transition dir-rtl cursor-default">
+                            <i class="w-6 text-center fal fa-cog pb-1"></i>
+                            <span class="variable-font-medium mr-2">@lang('Settings')</span>
+                        </div>
+                        <a href="{{ route('dashboard.center.setting.session-platforms', $center->id) }}" class="flex items-center text-sm text-gray-700 py-3 pl-4 pr-10 hover:bg-gray-100 border-b border-gray-100 transition dir-rtl">
+                            <i class="w-6 text-center fal fa-map-marker-alt pb-1 pl-2"></i>
+                            <span>@lang('Platform settings')</span>
+                        </a>
+                        <a href="{{ route('dashboard.room.setting.tags.show', $center->id) }}" class="flex items-center text-sm text-gray-700 py-3 pl-4 pr-10 hover:bg-gray-100 border-b border-gray-100 transition dir-rtl">
+                            <i class="w-6 text-center fal fa-tags pb-1 pl-2"></i>
+                            <span>@lang('Tags settings')</span>
+                        </a>
+                    @else
+                        <a href="{{ route('dashboard.center.setting.session-platforms', $center->id) }}" class="flex items-center text-sm text-gray-700 py-3 px-4 hover:bg-gray-100 border-b border-gray-100 transition dir-rtl">
                         <i class="w-6 text-center fal fa-cog pb-1"></i>
                         <span class="vaiable-font-medium mr-2">@lang('Settings')</span>
                     </a>
+                    @endif
                 @endcan
 
             </div>
