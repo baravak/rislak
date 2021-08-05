a26437618a33dfac6176ae0e817be9b7e0579b96
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-08-05 12:42:38 +0430

    Update center buttons
    Co-Authored-By: Hasan Salehi <953471+baravak@users.noreply.github.com>

--- resources/views/dashboard/centers/acceptationButton.blade.php
+++ resources/views/dashboard/centers/acceptationButton.blade.php
@@ -1,4 +1,7 @@
-<div data-xhr="center-acceptation-button">
+<div class="flex" data-xhr="center-acceptation-button">
+    <a href="{{ route('dashboard.center.schedules.index', $center->id) }}" class="flex items-center justify-center border border-gray-300 rounded-full h-9 w-9 hover:bg-gray-100 transition text-gray-400 focus mr-2" title="@lang('Therapy Schedules')" aria-label="@lang('Therapy Schedules')">
+        <i class="fal fa-calendar-alt"></i>
+    </a>
     @can('acceptation', $center)
     <a href="{{route('dashboard.centers.request', $center->id)}}" data-lijax="click" data-method="POST" class="flex justify-center items-center flex-shrink-0 text-white bg-green-600 hover:bg-green-700 w-auto px-4 sm:px-8 h-9 rounded-full text-xs sm:text-sm transition spinner">
         <span class="font-medium">{{ __('Acceptation request') }}</span>
--- resources/views/dashboard/centers/showButtons.blade.php
+++ resources/views/dashboard/centers/showButtons.blade.php
@@ -1,6 +1,5 @@
 <div class="absolute top-3 left-3 flex items-center dir-ltr" data-xhr="center-button">
-
-    @if (!$center->acceptation || ($center->acceptation && !in_array($center->acceptation->position, ['client', 'psychologist'])) || auth()->user()->isAdmin())
+    @if (($center->acceptation && !in_array($center->acceptation->position, ['client', 'psychologist'])) || auth()->user()->isAdmin())
         <div class="relative dropdown">
             <button type="button" class="flex items-center justify-center border border-gray-300 rounded-full h-9 w-9 hover:bg-gray-100 transition text-2xl text-gray-400 focus mr-2 dropdown-toggle">
                 <i class="fal fa-ellipsis-v"></i>
@@ -12,13 +11,6 @@
                         <span class="vaiable-font-medium mr-2">@lang('Users')</span>
                     </a>
                 @endcan
-
-                {{-- اگر شرط های پایین برقرار شد، این قسمت باید حذف شود --}}
-                    <a href="{{ route('dashboard.center.schedules.index', $center->id) }}" class="flex items-center text-sm text-gray-700 py-3 px-4 hover:bg-gray-100 border-b border-gray-100 transition dir-rtl">
-                        <i class="w-6 text-center fal fa-calendar-alt pb-1"></i>
-                        <span class="vaiable-font-medium mr-2">@lang('Therapy Schedules')</span>
-                    </a>
-                {{-- / اگر شرط های پایین برقرار شد، این قسمت باید حذف شود --}}
 
                 @if (auth()->center($center->id))
                     <a href="{{ route('dashboard.center.users.show', ['center' => $center->id, 'user' => $center->acceptation->id]) }}" class="flex items-center text-sm text-gray-700 py-3 px-4 hover:bg-gray-100 border-b border-gray-100 transition dir-rtl">
@@ -52,42 +44,24 @@
                     </a>
                     @endif
                 @endcan
-
             </div>
         </div>
 
-        {{-- اگر کاربر غیر از مراجع بود دکمه سه نقطه + دکمه برنامه درمانی را کنار هم می‌بیند --}}
-        @if (false)
+        @can('update', $center)
             <a href="{{ route('dashboard.center.schedules.index', $center->id) }}" class="flex justify-center items-center flex-shrink-0 text-brand hover:text-white hover:bg-brand border border-brand w-auto px-4 sm:px-8 h-9 rounded-full text-xs sm:text-sm transition focus">
                 <span class="vaiable-font-medium">@lang('Therapy Schedules')</span>
             </a>
-        @endif
-        {{--  / اگر کاربر غیر از مراجع بود دکمه سه نقطه + دکمه برنامه درمانی را کنار هم می‌بیند --}}
-    @endif
+        @endcan
 
-    @include('dashboard.centers.acceptationButton')
+        @elseif(!$center->acceptation)
+            @include('dashboard.centers.acceptationButton')
+        @else
 
-    {{-- اگر مراجع عضو مرکز نباشد --}}
-    @if (false)
-        <a href="{{ route('dashboard.center.schedules.index', $center->id) }}" class="flex items-center justify-center border border-gray-300 rounded-full h-9 w-9 hover:bg-gray-100 transition text-gray-400 focus mr-2" title="@lang('Therapy Schedules')" aria-label="@lang('Therapy Schedules')">
-            <i class="fal fa-calendar-alt"></i>
-        </a>
-
-        <a href="{{route('dashboard.centers.request', $center->id)}}" data-lijax="click" data-method="POST" class="flex justify-center items-center flex-shrink-0 text-white bg-green-600 hover:bg-green-700 w-auto px-4 sm:px-8 h-9 rounded-full text-xs sm:text-sm transition spinner">
-            <span class="font-medium">{{ __('Acceptation request') }}</span>
-        </a>
-    @endif
-    {{-- / اگر مراجع عضو مرکز نباشد --}}
-
-    {{-- اگر مراجع عضو مرکز باشد --}}
-    @if ($center->acceptation && in_array($center->acceptation->position, ['client', 'psychologist']) && !auth()->user()->isAdmin())
         <a href="{{ route('dashboard.center.users.show', ['center' => $center->id, 'user' => $center->acceptation->id]) }}" class="flex items-center justify-center border border-gray-300 rounded-full h-9 w-9 hover:bg-gray-100 transition text-gray-400 focus mr-2" title="@lang('My profile')" aria-label="@lang('My profile')">
             <i class="fal fa-user"></i>
         </a>
-
         <a href="{{ route('dashboard.center.schedules.index', $center->id) }}" class="flex justify-center items-center flex-shrink-0 text-brand hover:text-white hover:bg-brand border border-brand w-auto px-4 sm:px-8 h-9 rounded-full text-xs sm:text-sm transition focus">
             <span class="vaiable-font-medium">@lang('Therapy Schedules')</span>
         </a>
     @endif
-    {{-- / اگر مراجع عضو مرکز باشد --}}
 </div>
