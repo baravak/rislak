ee9dc68a6b15fbe4f196c518ceeb16d3e0fb3314
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-08-23 11:05:30 +0430

    Update schedules show

--- resources/views/dashboard/schedules/show.blade.php
+++ resources/views/dashboard/schedules/show.blade.php
@@ -1,5 +1,20 @@
 @extends($layouts->dashboard)
 @section('content')
-    @include('dashboard.sessions.newSessionDetails')
+    <div class="m-auto w-full md:w-2/3 xl:w-1/2 border border-gray-300 rounded p-4 mb-2 bg-gray-50 cursor-default">
+        <div class="flex items-center flex-wrap cursor-default mb-2">
+            <h3 class="text-gray-900 variable-font-semibold">رزرو @lang('Therapy session') {{ $session->id }}</h3>
+        </div>
+        <div class="flex flex-col sm:flex-row sm:items-center sm:justify-between">
+            <div class="flex items-start text-sm text-gray-700">
+                <i class="fal fa-calendar-alt ml-2 w-4 pb-1"></i>
+                <span class="variable-font-medium">@time($session->started_at, '%A %d %B %y ، ساعت H:i')</span>
+            </div>
+            <div class="flex items-start text-sm text-gray-700 mt-2 sm:mt-0">
+                <i class="fal fa-clock ml-2 w-4 pb-1"></i>
+                <span>{{ __(':time minute(s)', ['time' => $session->duration]) }}</span>
+            </div>
+        </div>
+        <div class="text-sm variable-font-light text-gray-500 mt-2 cursor-default">{{ $session->description }}</div>
+    </div>
     @include('dashboard.sessions.createUserForm')
 @endsection
--- resources/views/dashboard/sessions/createUserForm.blade.php
+++ resources/views/dashboard/sessions/createUserForm.blade.php
@@ -1,4 +1,4 @@
-<div class="m-auto w-full md:w-1/2 mt-4">
+<div class="m-auto w-full md:w-2/3 xl:w-1/2">
     <form class="w-full" data-paymental method="POST" action="{{ route($session->status =='registration_awaiting' ? 'dashboard.schedules.booking' : 'dashboard.session.users.store', $session->id) }}" {{ isset($callbackPayment) ? 'data-autosubmit' : '' }}>
         @method('POST')
         <div class="border border-gray-300 rounded p-4">
@@ -9,6 +9,5 @@
             @endcan
         </div>
         <button type="submit" class="inline-flex justify-center items-center h-9 px-8 bg-brand text-white text-sm rounded-full hover:bg-brand-600 transition ml-4 focus mt-4" role="button">تأیید و ثبت جلسه درمانی</button>
-
     </form>
 </div>
