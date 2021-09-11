280f04521972869407ebaacdf828402fa46f6445
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-09-11 21:25:07 +0430

    Update accounting index

--- resources/views/dashboard/centers/accounting/index.blade.php
+++ resources/views/dashboard/centers/accounting/index.blade.php
@@ -1,15 +1,21 @@
 @extends($layouts->dashboard)
 @section('content')
-
-    <ul data-tabs>
-        <li><a href="#commission" data-tabby-default class="direct focus flex" title="{{ __('commission') }}">{{ __('commission') }}</a></li>
-        <li><a href="#financial-balance" class="direct focus flex" title="{{ __('Financial-balance') }}">{{ __('Financial-balance') }}</a></li>
-    </ul>
-
-    <div id="commission">
-        @include('dashboard.centers.accounting.commission.index')
-    </div>
-    <div id="financial-balance">
-        @include('dashboard.centers.accounting.financialBalance')
+    <div class="w-full xs:w-2/3 xl:w-1/2 2xl:w-2/5 mx-auto grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-4 gap-4 mt-8">
+        <a href="#" class="border border-gray-300 hover:border-brand text-brand rounded p-4 flex xs:flex-col items-center xs:justify-center hover:bg-blue-100 transition group focus-current ring-brand">
+            <i class="fal fa-abacus text-2xl"></i>
+            <span class="mr-4 xs:mr-0 xs:mt-2 variable-font-medium text-sm text-gray-500 text-center">@lang('Commission')</span>
+        </a>
+        <a href="#" class="border border-gray-300 hover:border-yellow-500 text-yellow-500 rounded p-4 flex xs:flex-col items-center xs:justify-center hover:bg-yellow-50 transition group focus-current ring-yellow-500">
+            <i class="fal fa-balance-scale text-2xl"></i>
+            <span class="mr-4 xs:mr-0 xs:mt-2 variable-font-medium text-sm text-gray-500 text-center">@lang('Financial-balance')</span>
+        </a>
+        <a href="#" class="border border-gray-300 hover:border-green-600 text-green-600 rounded p-4 flex xs:flex-col items-center xs:justify-center hover:bg-green-50 transition group focus-current ring-green-600">
+            <i class="fal fa-cash-register text-2xl"></i>
+            <span class="mr-4 xs:mr-0 xs:mt-2 variable-font-medium text-sm text-gray-500 text-center">@lang('Checkout')</span>
+        </a>
+        <a href="#" class="border border-gray-300 hover:border-gray-600 text-gray-500 rounded p-4 flex xs:flex-col items-center xs:justify-center hover:bg-gray-50 transition group focus-current ring-gray-500">
+            <i class="fal fa-exchange text-2xl"></i>
+            <span class="mr-4 xs:mr-0 xs:mt-2 variable-font-medium text-sm text-gray-500 text-center">@lang('Credit transfer')</span>
+        </a>
     </div>
 @endsection
