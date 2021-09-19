9803cc878f542d96068289479ad9332953213bed
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-09-19 13:12:15 +0430

    Update create billing

--- resources/views/dashboard/billings/create.blade.php
+++ resources/views/dashboard/billings/create.blade.php
@@ -1,62 +1,41 @@
-@extends($layouts->dashboard)
-@section('content')
-<form action="{{ route('dashboard.billings.store', $model->id) }}" method="post">
-<div class="overflow-x-auto">
-    <div class="align-middle inline-block min-w-full">
-        <div class="overflow-hidden border border-gray-200 rounded">
-            <table class="min-w-full divide-y divide-gray-200">
-                <thead class="bg-gray-50 cursor-default">
-                    <tr>
-                        <th class="px-3 py-2 text-right text-xs variable-font-medium text-gray-500" scope="col">{{ __('Title') }}</th>
-                        <th class="px-3 py-2 text-right text-xs variable-font-medium text-gray-500" scope="col">{{ __('طرف صورت‌حساب') }}</th>
-                        <th class="px-3 py-2 text-right text-xs variable-font-medium text-gray-500" scope="col">{{ __('موقعیت نسبت به فاکتور') }}</th>
-                        <th class="px-3 py-2 text-right text-xs variable-font-medium text-gray-500" scope="col">{{ __('Amount') }} <small>تومان</small></th>
-                    </tr>
-                </thead>
-                <tbody class="divide-y divide-gray-200">
-                    <tr data-xhr="transaction-list-id" class="transition hover:bg-gray-50">
-                        <td class="px-3 py-2 whitespace-nowrap">
-                            <div class="flex items-center">
-                                <input placeholder="@lang('Title')" type="text" name="title" id="title" autocomplete="off" class="border border-gray-500 text-xs text-gray-600 h-8 w-full rounded px-2 focus:border-brand focus">
-                            </div>
-                        </td>
-                        <td class="p-3 whitespace-nowrap">
-                            <div>
-                                <select id="user_id" name="user_id" class="border border-gray-500 h-8 rounded pl-4 pr-8 w-full text-xs focus:border-brand focus">
-                                    @foreach ($users as $user)
-                                        <option value="{{ $user->id }}">{{ $user->name }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                        </td>
-                        <td class="px-3 py-2 whitespace-nowrap">
-                            <div>
-                                <select id="type" name="type" class="border border-gray-500 h-8 rounded pl-4 pr-8 w-full text-xs focus:border-brand focus">
-                                    <option value="debtor">بدهکار</option>
-                                    <option value="creditor">بستانکار</option>
-                                </select>
-                            </div>
-                        </td>
-                        <td class="p-3 whitespace-nowrap">
-                            <div>
-                                <input type="number"  name="amount" id="amount" autocomplete="off" class="border border-gray-500 h-8 rounded px-2 w-full text-xs focus:border-brand focus">
-                            </div>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+@extends('dashboard.create')
+@section('form_content')
+    <div>
+        <div>
+            <label for="title" class="block mb-2 text-sm text-gray-700 variable-font-medium">@lang('Title')</label>
+            <input type="text" name="title" id="title" autocomplete="off" class="border border-gray-500 text-sm text-gray-600 h-10 w-full rounded px-2 focus:border-brand focus">
+        </div>
+        <div class="mt-4">
+            <label for="user_id" class="block mb-2 text-sm text-gray-700 variable-font-medium">@lang('طرف صورت‌حساب')</label>
+            <select id="user_id" name="user_id" class="border border-gray-500 h-10 rounded pl-4 pr-8 w-full text-sm focus:border-brand focus">
+                {{-- @foreach ($users as $user)
+                    <option value="{{ $user->id }}">{{ $user->name }}</option>
+                @endforeach --}}
+                <option value="">محمدعلی نخلی</option>
+            </select>
+        </div>
+        <div class="mt-4">
+            <label for="type" class="block mb-2 text-sm text-gray-700 variable-font-medium">@lang('موقعیت نسبت به فاکتور')</label>
+            <select id="type" name="type" class="border border-gray-500 h-10 rounded pl-4 pr-8 w-full text-sm focus:border-brand focus">
+                <option value="debtor">@lang('Debtor')</option>
+                <option value="creditor">@lang('Creditor')</option>
+            </select>
+        </div>
+        <div class="mt-4">
+            <label for="treasury" class="block mb-2 text-sm text-gray-700 variable-font-medium">@lang('خزانه صورت‌حساب')</label>
+            <select id="treasury" name="treasury" class="border border-gray-500 h-10 rounded pl-4 pr-8 w-full text-sm focus:border-brand focus">
+                {{-- @foreach ($treasuries as $treasury)
+                    <option value="{{ $treasury->id }}">{{ $treasury->title }}</option>
+                @endforeach --}}
+                <option value="">خزانه‌ی نمونه</option>
+            </select>
+        </div>
+        <div class="mt-4">
+            <label for="amount" class="block mb-2 text-sm text-gray-700 variable-font-medium">@lang('Amount')</label>
+            <div class="w-full relative">
+                <input type="number" name="amount" id="amount" autocomplete="off" class="border border-gray-400 h-10 rounded pr-4 pl-20 w-full text-sm dir-ltr focus">
+                <span class="absolute left-1 top-1 text-sm flex items-center justify-center bg-gray-200 px-4 h-8 rounded cursor-default">@lang('Toman')</span>
+            </div>
         </div>
     </div>
-</div>
-خزانه صورت‌حساب
-<select name="treasury">
-    @foreach ($treasuries as $treasury)
-        <option value="{{ $treasury->id }}">{{ $treasury->title }}</option>
-    @endforeach
-</select>
-
-<button type="submit">
-ذخیره
-</button>
-</form>
 @endsection
