7c22c6c4474dd6177550ccf3e91ba9ad4b6b3b6c
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-09-20 16:22:51 +0430

    Update bank accounts

--- resources/views/dashboard/users/accounting/bankAccounts/listRaw.blade.php
+++ resources/views/dashboard/users/accounting/bankAccounts/listRaw.blade.php
@@ -13,12 +13,252 @@
     <td class="px-3 py-2 whitespace-nowrap">
         <div class="flex items-center relative top-0.5">
             {{-- <span class="text-xs text-gray-600 block cursor-default">در انتظار</span> --}}
+            <span class="text-xs text-green-600 block cursor-default">تأیید شده</span>
+            {{-- <span class="text-xs text-red-600 block cursor-default">تأیید نشده</span> --}}
+        </div>
+    </td>
+    <td class="px-3 py-2 whitespace-nowrap">
+        {{-- <div class="flex items-center text-xs text-gray-600 cursor-default">
+            <span class="variable-font-medium ml-1">@lang('Description'):</span>
+            <span>علت عدم تایید شماره شبا در این قسمت نوشته می‌شود.</span>
+        </div> --}}
+        <div class="flex items-center text-xs text-gray-600 cursor-default">
+            <span class="variable-font-medium ml-1">@lang('نام صاحب حساب'):</span>
+            <span>محمدعلی نخلی</span>
+            <span class="mx-2">-</span>
+            <span>بانک ملت</span>
+        </div>
+    </td>
+    <td class="px-3 p-3 whitespace-nowrap text-left dir-ltr">
+        <div class="flex items-center">
+            <a href="#" aria-label="{{ __('Edit') }}"><i class="fal fa-edit text-sm text-gray-600 hover:text-blue-600 relative top-0.5"></i></a>
+        </div>
+    </td>
+</tr>
+
+<tr class="transition hover:bg-gray-50">
+    <td class="px-3 py-2 whitespace-nowrap">
+        <div class="flex items-center">
+            <span class="text-xs text-gray-600 block text-right dir-ltr en cursor-default">IR0696000000010324200001</span>
+        </div>
+    </td>
+    <td class="px-3 py-2 whitespace-nowrap">
+        <div class="flex flex-col justify-center">
+            <span class="text-xs text-gray-600 variable-font-medium cursor-default">سهیلا احمدی</span>
+            <span class="text-xs text-gray-500 cursor-default mt-1">مرکز مشاوره خانواده</span>
+        </div>
+    </td>
+    <td class="px-3 py-2 whitespace-nowrap">
+        <div class="flex items-center relative top-0.5">
+            <span class="text-xs text-gray-600 block cursor-default">در انتظار</span>
+            {{-- <span class="text-xs text-green-600 block cursor-default">تأیید شده</span> --}}
+            {{-- <span class="text-xs text-red-600 block cursor-default">تأیید نشده</span> --}}
+        </div>
+    </td>
+    <td class="px-3 py-2 whitespace-nowrap">
+        {{-- <div class="flex items-center text-xs text-gray-600 cursor-default">
+            <span class="variable-font-medium ml-1">@lang('Description'):</span>
+            <span>علت عدم تایید شماره شبا در این قسمت نوشته می‌شود.</span>
+        </div> --}}
+        {{-- <div class="flex items-center text-xs text-gray-600 cursor-default">
+            <span class="variable-font-medium ml-1">@lang('نام صاحب حساب'):</span>
+            <span>محمدعلی نخلی</span>
+            <span class="mx-2">-</span>
+            <span>بانک ملت</span>
+        </div> --}}
+    </td>
+    <td class="px-3 p-3 whitespace-nowrap text-left dir-ltr">
+        <div class="flex items-center">
+            <a href="#" aria-label="{{ __('Edit') }}"><i class="fal fa-edit text-sm text-gray-600 hover:text-blue-600 relative top-0.5"></i></a>
+        </div>
+    </td>
+</tr>
+
+<tr class="transition hover:bg-gray-50">
+    <td class="px-3 py-2 whitespace-nowrap">
+        <div class="flex items-center">
+            <span class="text-xs text-gray-600 block text-right dir-ltr en cursor-default">IR0696000000010324200001</span>
+        </div>
+    </td>
+    <td class="px-3 py-2 whitespace-nowrap">
+        <div class="flex flex-col justify-center">
+            <span class="text-xs text-gray-600 variable-font-medium cursor-default">جواد افشاری مقدم</span>
+            <span class="text-xs text-gray-500 cursor-default mt-1">اتاق درمان شخصی کاظم عبداللهی</span>
+        </div>
+    </td>
+    <td class="px-3 py-2 whitespace-nowrap">
+        <div class="flex items-center relative top-0.5">
+            {{-- <span class="text-xs text-gray-600 block cursor-default">در انتظار</span> --}}
             {{-- <span class="text-xs text-green-600 block cursor-default">تأیید شده</span> --}}
             <span class="text-xs text-red-600 block cursor-default">تأیید نشده</span>
         </div>
     </td>
-    <td class="px-3 py-2 whitespace-nowrap"></td>
+    <td class="px-3 py-2 whitespace-nowrap">
+        <div class="flex items-center text-xs text-gray-600 cursor-default">
+            <span class="variable-font-medium ml-1">@lang('Description'):</span>
+            <span>علت عدم تایید شماره شبا در این قسمت نوشته می‌شود.</span>
+        </div>
+        {{-- <div class="flex items-center text-xs text-gray-600 cursor-default">
+            <span class="variable-font-medium ml-1">@lang('نام صاحب حساب'):</span>
+            <span>محمدعلی نخلی</span>
+            <span class="mx-2">-</span>
+            <span>بانک ملت</span>
+        </div> --}}
+    </td>
     <td class="px-3 p-3 whitespace-nowrap text-left dir-ltr">
-        <a href="#" aria-label="{{ __('Edit') }}"><i class="fal fa-edit text-sm text-gray-600 hover:text-blue-600 relative top-0.5"></i></a>
+        <div class="flex items-center">
+            <a href="#" aria-label="{{ __('Edit') }}"><i class="fal fa-edit text-sm text-gray-600 hover:text-blue-600 relative top-0.5"></i></a>
+        </div>
+    </td>
+</tr>
+
+<tr class="transition hover:bg-gray-50">
+    <td class="px-3 py-2 whitespace-nowrap">
+        <div class="flex items-center">
+            <span class="w-1 h-10 bg-yellow-300 rounded-full ml-2"></span>
+            <span class="text-xs text-gray-600 block text-right dir-ltr en cursor-default">IR0696000000010324200001</span>
+        </div>
+    </td>
+    <td class="px-3 py-2 whitespace-nowrap">
+        <div class="flex flex-col justify-center">
+            <span class="text-xs text-gray-600 variable-font-medium cursor-default">جواد افشاری مقدم</span>
+            <span class="text-xs text-gray-500 cursor-default mt-1">اتاق درمان شخصی کاظم عبداللهی</span>
+        </div>
+    </td>
+    <td class="px-3 py-2 whitespace-nowrap">
+        <div class="flex flex-col justify-center relative top-1">
+            <div>
+                <label class="inline-flex items-center group">
+                    <input checked type="radio" name="status" id="status-awaiting" value="awaiting" class="w-3.5 h-3.5 border border-gray-600 focus">
+                    <span class="text-xs text-gray-600 mr-2 group-hover:text-blue-600">@lang('Awaiting')</span>
+                </label>
+            </div>
+            <div>
+                <label class="inline-flex items-center group">
+                    <input type="radio" name="status" id="status-unverified" value="unverified" class="w-3.5 h-3.5 border border-gray-600 focus">
+                    <span class="text-xs text-gray-600 mr-2 group-hover:text-blue-600">@lang('Unverified')</span>
+                </label>
+            </div>
+            <div>
+                <label class="inline-flex items-center group">
+                    <input type="radio" name="status" id="status-verified" value="verified" class="w-3.5 h-3.5 border border-gray-600 focus">
+                    <span class="text-xs text-gray-600 mr-2 group-hover:text-blue-600">@lang('Verified')</span>
+                </label>
+            </div>
+        </div>
+    </td>
+    <td class="px-3 py-2 whitespace-nowrap">
+        <div class="flex flex-col justify-center">
+
+        </div>
+    </td>
+    <td class="px-3 p-3 whitespace-nowrap text-left dir-ltr">
+        <div class="flex items-center">
+            <a href="#" aria-label="@lang('Save')" class="flex items-center border border-green-600 h-6 px-4 text-green-600 hover:bg-green-600 hover:text-white transition text-xs rounded-full">@lang('Save')</a>
+        </div>
+    </td>
+</tr>
+
+<tr class="transition hover:bg-gray-50">
+    <td class="px-3 py-2 whitespace-nowrap">
+        <div class="flex items-center">
+            <span class="w-1 h-10 bg-yellow-300 rounded-full ml-2"></span>
+            <span class="text-xs text-gray-600 block text-right dir-ltr en cursor-default">IR0696000000010324200001</span>
+        </div>
+    </td>
+    <td class="px-3 py-2 whitespace-nowrap">
+        <div class="flex flex-col justify-center">
+            <span class="text-xs text-gray-600 variable-font-medium cursor-default">جواد افشاری مقدم</span>
+            <span class="text-xs text-gray-500 cursor-default mt-1">اتاق درمان شخصی کاظم عبداللهی</span>
+        </div>
+    </td>
+    <td class="px-3 py-2 whitespace-nowrap">
+        <div class="flex flex-col justify-center relative top-1">
+            <div>
+                <label class="inline-flex items-center group">
+                    <input type="radio" name="status1" id="status-awaiting" value="awaiting" class="w-3.5 h-3.5 border border-gray-600 focus">
+                    <span class="text-xs text-gray-600 mr-2 group-hover:text-blue-600">@lang('Awaiting')</span>
+                </label>
+            </div>
+            <div>
+                <label class="inline-flex items-center group">
+                    <input type="radio" name="status1" id="status-unverified" value="unverified" class="w-3.5 h-3.5 border border-gray-600 focus">
+                    <span class="text-xs text-gray-600 mr-2 group-hover:text-blue-600">@lang('Unverified')</span>
+                </label>
+            </div>
+            <div>
+                <label class="inline-flex items-center group">
+                    <input checked type="radio" name="status1" id="status-verified" value="verified" class="w-3.5 h-3.5 border border-gray-600 focus">
+                    <span class="text-xs text-gray-600 mr-2 group-hover:text-blue-600">@lang('Verified')</span>
+                </label>
+            </div>
+        </div>
+    </td>
+    <td class="px-3 py-2 whitespace-nowrap">
+        <div class="flex items-center">
+            <div>
+                <label for="name" class="block mb-1 text-xs text-gray-700 variable-font-medium">@lang('نام صاحب حساب')</label>
+                <input type="text" name="name" id="name" autocomplete="off" class="w-40 border border-gray-400 text-xs text-gray-600 h-8 rounded px-2 focus ml-2">
+            </div>
+            <div>
+                <label for="bank" class="block mb-1 text-xs text-gray-700 variable-font-medium">@lang('نام بانک')</label>
+                <input type="text" name="bank" id="bank" autocomplete="off" class="w-40 border border-gray-400 text-xs text-gray-600 h-8 rounded px-2 focus ml-2">
+            </div>
+        </div>
+    </td>
+    <td class="px-3 p-3 whitespace-nowrap text-left dir-ltr">
+        <div class="flex items-center">
+            <a href="#" aria-label="@lang('Save')" class="flex items-center border border-green-600 h-6 px-4 text-green-600 hover:bg-green-600 hover:text-white transition text-xs rounded-full">@lang('Save')</a>
+        </div>
+    </td>
+</tr>
+
+<tr class="transition hover:bg-gray-50">
+    <td class="px-3 py-2 whitespace-nowrap">
+        <div class="flex items-center">
+            <span class="w-1 h-10 bg-yellow-300 rounded-full ml-2"></span>
+            <span class="text-xs text-gray-600 block text-right dir-ltr en cursor-default">IR0696000000010324200001</span>
+        </div>
+    </td>
+    <td class="px-3 py-2 whitespace-nowrap">
+        <div class="flex flex-col justify-center">
+            <span class="text-xs text-gray-600 variable-font-medium cursor-default">جواد افشاری مقدم</span>
+            <span class="text-xs text-gray-500 cursor-default mt-1">اتاق درمان شخصی کاظم عبداللهی</span>
+        </div>
+    </td>
+    <td class="px-3 py-2 whitespace-nowrap">
+        <div class="flex flex-col justify-center relative top-1">
+            <div>
+                <label class="inline-flex items-center group">
+                    <input type="radio" name="status2" id="status-awaiting" value="awaiting" class="w-3.5 h-3.5 border border-gray-600 focus">
+                    <span class="text-xs text-gray-600 mr-2 group-hover:text-blue-600">@lang('Awaiting')</span>
+                </label>
+            </div>
+            <div>
+                <label class="inline-flex items-center group">
+                    <input checked type="radio" name="status2" id="status-unverified" value="unverified" class="w-3.5 h-3.5 border border-gray-600 focus">
+                    <span class="text-xs text-gray-600 mr-2 group-hover:text-blue-600">@lang('Unverified')</span>
+                </label>
+            </div>
+            <div>
+                <label class="inline-flex items-center group">
+                    <input type="radio" name="status2" id="status-verified" value="verified" class="w-3.5 h-3.5 border border-gray-600 focus">
+                    <span class="text-xs text-gray-600 mr-2 group-hover:text-blue-600">@lang('Verified')</span>
+                </label>
+            </div>
+        </div>
+    </td>
+    <td class="px-3 py-2 whitespace-nowrap">
+        <div class="flex items-center">
+            <div>
+                <label for="description" class="block mb-1 text-xs text-gray-700 variable-font-medium">@lang('Description')</label>
+                <input type="text" name="description" id="description" placeholder="@lang('علت عدم تایید شماره شبا را بنویسید')" autocomplete="off" class="w-80 border border-gray-400 text-xs text-gray-600 h-8 rounded px-2 focus placeholder-gray-300">
+            </div>
+        </div>
+    </td>
+    <td class="px-3 p-3 whitespace-nowrap text-left dir-ltr">
+        <div class="flex items-center">
+            <a href="#" aria-label="@lang('Save')" class="flex items-center border border-green-600 h-6 px-4 text-green-600 hover:bg-green-600 hover:text-white transition text-xs rounded-full">@lang('Save')</a>
+        </div>
     </td>
 </tr>
