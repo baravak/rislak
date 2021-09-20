9826cff7285faccee01f927e1cef348c9e501e89
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-09-20 20:26:06 +0430

    Minor fix

--- resources/views/dashboard/treasuries/index.blade.php
+++ resources/views/dashboard/treasuries/index.blade.php
@@ -2,16 +2,16 @@
 @section('content')
     <div>
         <div class="mt-8 mb-4">
-            <h3 class="heading" data-total="(213)" data-xhr="total">@lang('Bank accounts')</h3>
+            <h3 class="heading" data-total="({{ $treasuries ? $treasuries->total() : 0 }})" data-xhr="total">{{ __('Financial treasuries') }}</h3>
         </div>
 
-        <div class="flex items-center mb-4">
-            @include('layouts.quick_search')
-            {{-- <a href="#" class="flex items-center justify-center flex-shrink-0 w-9 sm:w-auto h-9 sm:px-4 text-sm text-green-700 border border-green-700 rounded-full hover:bg-green-50 transition focus-current ring-green-700 mr-4" title="{{ __('Create new bank account') }}" aria-label="title={{ __('Create new bank account') }}">
+        <div class="flex items-center justify-between mb-4">
+            <div></div>
+            <a href="{{ route('dashboard.treasuries.create') }}" class="flex items-center justify-center flex-shrink-0 w-9 sm:w-auto h-9 sm:px-4 text-sm text-green-700 border border-green-700 rounded-full hover:bg-green-50 transition focus-current ring-green-700 mr-4" title="@lang('Create new Treasury')" aria-label="title="@lang('Create new Treasury')">
                 <i class="fal fa-plus sm:ml-2"></i>
-                <span class="hidden sm:inline">{{ __('Create new bank account') }}</span>
-            </a> --}}
+                <span>@lang('Create new Treasury')</span>
+            </a>
         </div>
-        @include('dashboard.users.accounting.bankAccounts.list')
+        @include('dashboard.treasuries.list')
     </div>
 @endsection
