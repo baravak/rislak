ad708ace4c5574f1f541c336ce9cf3defdf79f96
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-09-22 12:48:57 +0330

    Minor fix

--- resources/views/dashboard/centers/accounting/bank/addBankAccounts.blade.php
+++ resources/views/dashboard/centers/accounting/bank/addBankAccounts.blade.php
@@ -10,7 +10,7 @@
                     <div class="w-full relative" x-data='{"IBAN" : null, "IBANString" : null}'>
                         <input  type="number" name="isbn" :value="IBAN" x-model="IBAN" x-effect="IBANString = IBAN ?`IR-${IBAN.substr(0,2)}${'0'.repeat(2 - IBAN.substr(0,2).length)}-${IBAN.substr(2,3)}${'0'.repeat(3 - IBAN.substr(2,3).length)}-${IBAN.substr(5,1) || '0'}-${IBAN.substr(6, 18)}${'0'.repeat(18 - IBAN.substr(6,18).length)}` : null" id="sheba" class="border border-gray-300 h-10 rounded pr-4 pl-14 w-full text-sm dir-ltr focus">
                         <span class="absolute left-1 top-1 text-sm flex items-center justify-center bg-gray-200 rounded px-4 h-8 cursor-default pt-0.5">@lang('IR')</span>
-                        <span class="block text-sm text-gray-500 dir-ltr text-left mt-2" x-text="IBANString"></span>
+                        <span class="block text-sm text-gray-500 dir-ltr text-left mt-2 cursor-default" x-text="IBANString"></span>
                     </div>
                     <div class="mt-6 flex justify-end">
                         <button type="submit" class="bg-brand rounded-full h-9 px-8 text-white text-sm hover:bg-blue-700 transition focus">
--- resources/views/dashboard/centers/accounting/bank/checkoutNotAvailable.blade.php
+++ resources/views/dashboard/centers/accounting/bank/checkoutNotAvailable.blade.php
@@ -1,3 +1,3 @@
-<div class="flex items-center justify-center text-center text-sm text-gray-600 leading-6 h-32">
+<div class="flex items-center justify-center text-center text-sm text-gray-600 leading-6 h-32 cursor-default">
     <p>شما هیچ حساب بانکی فعالی ندارید.<br>ابتدا شماره شبا حساب بانکی خود را از طریق افزودن حساب ثبت کنید. پس از تأیید ریسلو، در این بخش روند تسویه را انجام دهید.</p>
 </div>
