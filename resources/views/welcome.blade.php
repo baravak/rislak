74e613afec715e8143c318c287b033a3bacd6dc5
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-10-31 17:14:25 +0330

    Update public

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
     <title>Risloo | ریسلو</title>
 </head>
 
-<body class="flex flex-col text-gray-900 bg-gray-50">
+<body class="flex flex-col text-gray-900">
     <main class="flex-1 pt-32" style="padding-bottom: 4rem;">
         <nav class="bg-white shadow py-4 fixed top-0 right-0 left-0 w-full">
             <div class="container mx-auto px-4">
@@ -35,15 +35,18 @@
         <section class="px-4">
             <h2 class="text-center text-2xl sm:text-4xl variable-font-bold">درمان، آموزش و پژوهش</h2>
             <p class="text-center sm:w-1/2 md:w-1/3 mt-4 mx-auto">شما به عنوان روان‌شناس، روی کار اصلی خود تمرکز کنید؛ دغدغه کارهای جانبی <span class="variable-font-bold">درمان، آموزش و پژوهش</span> را نداشته باشید و به خود آن‌ها فقط فکر کنید. ریسلو بستری یک‌پارچه است که کارهای شما را در این سه حوزه تسهیل می‌کند.</p>
-            <div class="flex flex-col justify-center items-center text-center mt-8">
-                <a href="/register" class="bg-brand text-white py-2 px-8 rounded-full">عضو ریسلو شوید</a>
-                <a href="https://play.google.com/store/apps/details?id=com.majazeh.risloo" target="_blank" rel="noopener" class="variable-font-normal text-gray-700 hover:text-gray-900 transition mt-4">دانلود نسخه اندروید از <span class="variable-font-bold">Google Play</span></a>
+            <div class="flex justify-center items-center text-center mt-8">
+                <a href="/register" class="flex items-center bg-brand text-white h-12 px-6 ml-4 rounded-lg">عضو ریسلو شوید</a>
+                {{-- <a href="https://play.google.com/store/apps/details?id=com.majazeh.risloo" target="_blank" rel="noopener" class="variable-font-normal text-gray-700 hover:text-gray-900 transition mt-4">دانلود نسخه اندروید از <span class="variable-font-bold">Google Play</span></a> --}}
+                <a href="https://play.google.com/store/apps/details?id=com.majazeh.risloo" target="_blank" rel="noopener" class="text-gray-700 hover:text-gray-900 transition">
+                    <img class="h-12" src="{{ asset('/images/graphics/google-play.png') }}" alt="Google Play">
+                </a>
             </div>
         </section>
 
         <section class="mt-8 px-4">
-            <div class="w-full lg:w-1/2 mx-auto shadow">
-                <img src="{{ asset('/public/images/main.jpg') }}" alt="ریسلو">
+            <div class="w-full lg:w-1/2 mx-auto">
+                <img src="{{ asset('/images/graphics/risloo-main.jpg') }}" alt="ریسلو">
             </div>
         </section>
     </main>
