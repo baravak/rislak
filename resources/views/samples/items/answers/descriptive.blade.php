60a3b04e486904f01da7175a1ca074f8b9a1d3f1
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-11-08 12:28:23 +0330

    Add descriptive answer

---
+++ resources/views/samples/items/answers/descriptive.blade.php
@@ -0,0 +1,9 @@
+<div>
+    <textarea name="" id="" placeholder="پاسخ را در این قسمت بنویسید" class="w-full text-sm text-gray-600 h-48 border border-gray-300 rounded resize-none placeholder-gray-300 p-4"></textarea>
+    <div class="flex items-center justify-end cursor-default">
+        {{-- <span class="text-sm variable-font-medium text-red-500">242</span> --}}
+        <span class="text-sm variable-font-medium text-gray-600">0</span>
+        <span class="text-xs text-gray-400 mx-1">/</span>
+        <span class="text-xs text-gray-400">250</span>
+    </div>
+</div>
