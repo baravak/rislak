ead043df1cf1e8e66a4b7cae006a9149ca6896cc
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-11-09 13:10:29 +0330

    Minor fix
    Co-Authored-By: Hasan Salehi <953471+baravak@users.noreply.github.com>

--- resources/views/dashboard/samples/do.blade.php
+++ resources/views/dashboard/samples/do.blade.php
@@ -1,13 +1,13 @@
 @can('management', [$sample, isset($room) ? $room : null])
-    <div class="inline-block mr-4">
+    <div class="inline-block mr-4  relative top-0.5">
         <a href="{{ urldecode(route('dashboard.samples.show', $sample->id)) }}" title="@lang('View')" aria-label="@lang('View')">
-            <i class="fal fa-eye text-sm text-gray-600 hover:text-blue-600 relative top-0.5"></i>
+            <i class="fal fa-eye text-sm text-gray-600 hover:text-blue-600"></i>
         </a>
     </div>
 @endcan
 
 @if ($sample->chain)
-    <div class="inline-block mr-6">
+    <div class="inline-block mr-6 relative top-0.5">
         <a href="{{ route('dashboard.samples.index', ['chain' => $sample->chain->id]) }}" class="relative text-gray-600 hover:text-blue-600" title="@lang('نمو‌نه‌های زنجیره‌ای این نمونه')" aria-label="@lang('نمو‌نه‌های زنجیره‌ای این نمونه')">
             <i class="fal fa-ballot-check relative top-0.5"></i>
             <i class="fal fa-link text-xs flex items-center justify-center w-4 h-4 shadow bg-white rounded-full absolute left-2 bottom-0"></i>
@@ -20,3 +20,15 @@
         <a href="{{ urldecode(route('samples.form', $sample->id)) }}" target="_blank" class="inline-flex items-center justify-center w-24 h-7 text-xs text-blue-600 hover:text-white border border-blue-600 hover:bg-blue-600 rounded-full transition">{{ __('Do sample') }}</a>
     </div>
 @endcan
+
+@if ($sample->status == 'closed')
+    @can('management', [$sample, isset($room) ? $room : null])
+        <span class="text-xs text-gray-500">
+            <form action="{!! urldecode(route('dashboard.samples.scoring', ['sample' => $sample->id, 'inline' => 1])) !!}" method="POST"  class="inline-block" data-lijax-preload="______">
+                <button type="submit" class="inline-flex items-center justify-center w-24 h-7 text-xs text-blue-600 hover:text-white border border-blue-600 hover:bg-blue-600 rounded-full transition">
+                    {{ __("Scoring") }}
+                </button>
+            </form>
+        </span>
+    @endcan
+@endif
--- resources/views/dashboard/samples/listRaw.blade.php
+++ resources/views/dashboard/samples/listRaw.blade.php
@@ -1,12 +1,25 @@
 <div data-xhr="sample-list-{{ $sample->id }}" class="flex items-center bg-gray-50 hover:bg-gray-100 transition py-3 sm:py-2 p-2 rounded mt-2">
-    <a href="{{ urldecode(route('dashboard.samples.show', $sample->id)) }}" class="w-24 text-xs text-gray-600 hover:text-blue-600 transition hidden lg:flex">
-        <span class="text-right dir-ltr en">{{ $sample->id }}</span>
-    </a>
-    <div class="flex-1 px-2 flex flex-col">
-        <a href="{{ urldecode(route('dashboard.samples.show', $sample->id)) }}" class="group text-xs text-gray-600 hover:text-blue-600 transition border-r-2 border-gray-400 sm:border-none hover:border-blue-600 pr-2 sm:pr-0">
-            <span class="block">{{ $sample->scale->title }}</span>
-            <span class="block text-gray-400 font-light group-hover:text-blue-600">{{$sample->edition ? __('Edition :title', ['title' => $sample->edition]) .' - ' : ''}} {{ __('Version :ver', ['ver' => $sample->version]) }}</span>
+    @can('management', [$sample, isset($room) ? $room : null])
+        <a href="{{ urldecode(route('dashboard.samples.show', $sample->id)) }}" class="w-24 text-xs text-gray-600 hover:text-blue-600 transition hidden lg:flex">
+            <span class="text-right dir-ltr en">{{ $sample->id }}</span>
         </a>
+    @else
+        <div class="w-24 hidden lg:flex text-xs text-gray-600 cursor-default">
+            <span class="text-right dir-ltr en">{{ $sample->id }}</span>
+        </div>
+    @endcan
+    <div class="flex-1 px-2 flex flex-col">
+        @can('management', [$sample, isset($room) ? $room : null])
+            <a href="{{ urldecode(route('dashboard.samples.show', $sample->id)) }}" class="group text-xs text-gray-600 hover:text-blue-600 transition border-r-2 border-gray-400 sm:border-none hover:border-blue-600 pr-2 sm:pr-0">
+                <span class="block">{{ $sample->scale->title }}</span>
+                <span class="block text-gray-400 font-light group-hover:text-blue-600">{{$sample->edition ? __('Edition :title', ['title' => $sample->edition]) .' - ' : ''}} {{ __('Version :ver', ['ver' => $sample->version]) }}</span>
+            </a>
+        @else
+            <div class="text-xs text-gray-600 border-r-2 border-gray-400 sm:border-none pr-2 sm:pr-0 cursor-default">
+                <span class="block">{{ $sample->scale->title }}</span>
+                <span class="block text-gray-400 font-light group-hover:text-blue-600">{{$sample->edition ? __('Edition :title', ['title' => $sample->edition]) .' - ' : ''}} {{ __('Version :ver', ['ver' => $sample->version]) }}</span>
+            </div>
+        @endcan
         <div class="mt-4 sm:hidden flex flex-col">
             <div class="flex">
                 <a href="{{ $sample->room->route('show') }}" class="text-xs text-gray-600 hover:text-blue-500 underline">{{ __('Therapy room of :user', ['user' => $sample->room->manager->name]) }}</a>
