59fc0684b0f8cab5686d68887b9564b2723a2456
abouhatef <itb.baravak@gmial.com>
2021-11-10 00:47:33 +0330

    other user status bugfix

--- resources/views/dashboard/samples/tables/status.blade.php
+++ resources/views/dashboard/samples/tables/status.blade.php
@@ -1,5 +1,4 @@
-
-@if (in_array($sample->status, ['scoring', 'creating_files']))
+@if (in_array($sample->status, ['scoring', 'creating_files']) && auth()->user()->can('management', [$sample, isset($room) ? $room : null]))
     <span class="text-xs text-gray-500 cursor-default" data-samplsta="{{ $sample->id }}" data-xhr="sample-status-{{ $sample->id }}" title="@lang('در حال نمره‌دهی')" aria-label="@lang('در حال نمره‌دهی')">
         <i class="fad fa-spinner-third animate-spin"></i>
     </span>
