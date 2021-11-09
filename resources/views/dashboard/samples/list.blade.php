4ef1208548e519a8297ce0b6e4746a988d8aa174
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-11-09 10:33:01 +0330

    Minor fix

--- resources/views/dashboard/samples/list.blade.php
+++ resources/views/dashboard/samples/list.blade.php
@@ -1,11 +1,11 @@
 <div data-xhr="sample-items">
     @if ($samples->count())
-        <div class="flex items-center cursor-default px-2 text-xs variable-font-medium text-gray-600 bg-gray-100 py-2 rounded">
+        <div class="flex items-center cursor-default px-2 text-xs variable-font-medium text-gray-600 bg-gray-100 py-2 rounded hidden sm:flex">
             <div class="w-24 hidden lg:flex">@lang('Serial')</div>
             <div class="flex-1 px-2">@lang('Scale')</div>
             <div class="flex-1 px-2 hidden md:flex">@lang('Client')</div>
-            <div class="flex-1 px-2 hidden sm:flex">@lang('Therapy room')</div>
-            <div class="flex-1 px-2 hidden sm:flex">@lang('Status')</div>
+            <div class="flex-1 px-2">@lang('Therapy room')</div>
+            <div class="flex-1 px-2">@lang('Status')</div>
             <div class="flex-1 px-2"></div>
         </div>
         @foreach ($samples as $sample)
