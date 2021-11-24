9bb147e2be990f50328d4185133010a4c1c8ab8f
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-11-24 20:13:41 +0330

    Update gift list responsive (not completed)

--- resources/views/dashboard/gifts/list.blade.php
+++ resources/views/dashboard/gifts/list.blade.php
@@ -1,19 +1,19 @@
 <div data-xhr="gift-items">
     {{-- @if ($gifts && $gifts->count()) --}}
-        <div class="hidden sm:flex items-center cursor-default px-2 text-xs variable-font-medium text-gray-600 bg-gray-100 py-2 rounded">
-            <div class="flex-1 pl-2">@lang('Code')</div>
+        <div class="hidden md:flex items-center cursor-default px-2 text-xs variable-font-medium text-gray-600 bg-gray-100 py-2 rounded">
+            <div class="w-40 pl-2">@lang('Code')</div>
             <div class="flex-1 px-2">@lang('Title')</div>
-            <div class="flex-1 px-2">
+            <div class="flex-1 px-2 hidden lg:block">
                 <div>@lang('تاریخ شروع اعتبار')</div>
                 <div class="mt-1">@lang('تاریخ انقضا')</div>
             </div>
-            <div class="flex-1 px-2">
-                <div>@lang('تعداد دفعات مجاز')</div>
-                <div class="mt-1">@lang('تعداد دفعات قابل استفاده')</div>
+            <div class="flex-1 px-2 hidden lg:block">
+                <div>@lang('تعداد مجاز')</div>
+                <div class="mt-1">@lang('تعداد استفاده شده')</div>
             </div>
             <div class="flex-1 px-2">@lang('تعداد کاربران استفاده کننده')</div>
-            <div class="flex-1 px-2">@lang('کاربر خاص')</div>
-            <div class="flex-1 px-2">@lang('Status')</div>
+            <div class="flex-1 px-2 hidden lg:block">@lang('کاربر خاص')</div>
+            <div class="flex-1 px-2 hidden lg:block">@lang('Status')</div>
             <div class="flex-1 px-2"></div>
         </div>
         {{-- @foreach ($gifts as $gift) --}}
--- resources/views/dashboard/gifts/listRaw.blade.php
+++ resources/views/dashboard/gifts/listRaw.blade.php
@@ -1,26 +1,35 @@
 <div data-xhr="gift-list-id" class="flex items-center bg-gray-50 hover:bg-gray-100 transition py-3 sm:py-2 p-2 rounded mt-2">
-    <div class="flex items-center pl-2">
+    <div class="w-40 flex items-center pl-2">
         <a href="#" title="@lang('کپی کردن کد')"><i class="fal fa-copy text-sm text-gray-600 hover:text-blue-600 transition ml-2 pt-0.5"></i></a>
         <span class="text-xs text-right text-gray-600 dir-ltr en cursor-default">RS966666Q-G101SFGV</span>
     </div>
     <div class="flex-1 px-2 cursor-default">
-        <span class="text-xs text-gray-600">یلدا 1400</span>
+        <span class="text-xs text-gray-600 variable-font-medium lg:variable-font-normal">یلدا 1400</span>
+        <div class="text-xs text-gray-600 lg:hidden mt-1">شروع اعتبار: 1400/05/25</div>
+        <div class="text-xs text-gray-500 lg:hidden mt-1">انقضا: 1400/05/29</div>
+        <div class="text-xs lg:hidden mt-1">
+            <span class="text-gray-600">@lang('وضعیت')</span>
+            <span class="text-green-600">فعال</span>
+        </div>
     </div>
-    <div class="flex-1 px-2 cursor-default">
+    <div class="flex-1 px-2 cursor-default hidden lg:block">
         <div class="text-xs text-gray-600">1400/05/25</div>
         <div class="text-xs text-gray-500 mt-1">1400/05/29</div>
     </div>
-    <div class="flex-1 px-2 cursor-default">
+    <div class="flex-1 px-2 cursor-default hidden lg:block">
         <div class="text-xs text-gray-600">20</div>
         <div class="text-xs text-gray-600 mt-1">2</div>
     </div>
     <div class="flex-1 px-2 cursor-default">
-        <span class="text-xs text-gray-600">10</span>
+        <div class="text-xs text-gray-600">10</div>
+        <div class="text-xs text-gray-600 mt-1 lg:hidden">کاربر خاص: ندارد</div>
+        <div class="text-xs text-gray-600 mt-1 lg:hidden">تعداد مجاز: 20</div>
+        <div class="text-xs text-gray-600 mt-1 lg:hidden">تعداد استفاده شده: 2</div>
     </div>
-    <div class="flex-1 px-2 cursor-default">
-        <span class="text-xs text-gray-600">-</span>
+    <div class="flex-1 px-2 cursor-default hidden lg:block">
+        <span class="text-xs text-gray-600">ندارد</span>
     </div>
-    <div class="flex-1 px-2 cursor-default">
+    <div class="flex-1 px-2 cursor-default hidden lg:block">
         <span class="text-xs text-green-600">فعال</span>
         {{-- <span class="text-xs text-yellow-500">در انتظار</span> --}}
         {{-- <span class="text-xs text-red-600">منقضی شده</span> --}}
