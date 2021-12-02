58c8484751f1e2d37de9fb728e58fd9f766e1ddc
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-12-02 19:50:45 +0330

    Update public gift show

--- resources/views/dashboard/gifts/show.blade.php
+++ resources/views/dashboard/gifts/show.blade.php
@@ -1,103 +1,29 @@
-@extends($layouts->dashboard)
-
-@section('content')
-    <div>
-        <div class="grid grid-cols-12 gap-4">
-            <div class="col-span-1 hidden lg:block"></div>
-            @include('dashboard.gifts.shareSide')
-            <div class="col-span-full sm:col-span-7 lg:col-span-5 flex flex-col">
-                <div class="bg-gray-100 bg-opacity-50 border border-gray-200 rounded-lg p-4">
-                    <div class="grid grid-cols-2 gap-4 cursor-default">
-                        <div>
-                            <span class="block text-xs text-gray-500">تاریخ شروع اعتبار</span>
-                            <span class="block text-gray-700 variable-font-medium mt-1">@time($gift->started_at, 'Y/m/d')</span>
-                        </div>
-                        <div>
-                            <span class="block text-xs text-gray-500">تاریخ انقضا</span>
-                            @if ($gift->expires_at)
-                                <span class="block text-gray-700 variable-font-medium mt-1">@time($gift->expires_at, 'Y/m/d')</span>
-                            @else
-                                <i class="far fa-infinity text-xs text-gray-700 variable-font-medium mt-2" title="@lang('نامحدود')"></i>
-                            @endif
-                        </div>
-                        <div>
-                            <span class="block text-xs text-gray-500">تعداد مجاز</span>
-                            @if ($gift->threshold)
-                                <span class="block text-gray-700 variable-font-medium mt-1">{{ $gift->threshold }}</span>
-                            @else
-                                <i class="far fa-infinity text-xs text-gray-700 variable-font-medium mt-2" title="@lang('نامحدود')"></i>
-                            @endif
-                        </div>
-                        <div>
-                            <span class="block text-xs text-gray-500">تعداد استفاده شده</span>
-                            <span class="block text-gray-700 variable-font-medium mt-1">{{ $gift->usage_count }}</span>
-                        </div>
-                        <div>
-                            <span class="block text-xs text-gray-500">تعداد کاربران استفاده کننده</span>
-                            <span class="block text-gray-700 variable-font-medium mt-1">{{ $gift->user_count }}</span>
-                        </div>
-                        <div>
-                            <span class="block text-xs text-gray-500">کاربر خاص</span>
-                            <span class="block text-gray-700 variable-font-medium mt-1">{{ $gift->exclusive ? 'دارد' : 'ندارد' }}</span>
-                        </div>
-                        <div>
-                            <span class="block text-xs text-gray-500">تعداد استفاده توسط هر کاربر</span>
-                            <span class="block text-sm text-gray-700 variable-font-medium mt-1.5">{{ $gift->disposable ? 'بیش از یک‌بار' : 'فقط یک‌بار' }}</span>
-                        </div>
-                        <div>
-                            <span class="block text-xs text-gray-500">وضعیت</span>
-                            @switch($gift->status)
-                                @case('open')
-                                    <span class="block text-sm text-green-600 variable-font-medium mt-1.5">فعال</span>
-                                    @break
-                                @case('awaiting')
-                                    <span class="block text-sm text-yellow-500 variable-font-medium mt-1.5">در انتظار</span>
-                                    @break
-                                @case('expires')
-                                        <span class="block text-sm text-red-600 variable-font-medium mt-1.5">منقضی شده</span>
-                                    @break
-                            @endswitch
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="hidden lg:block lg:col-span-2 space-y-3">
-                @include('dashboard.gifts.options')
-            </div>
-            <div class="col-span-1 hidden lg:block"></div>
+<!DOCTYPE html>
+<html lang="fa">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>کد تخفیف</title>
+    <meta property="og:title" content="" />
+    <meta property="og:type" content="" />
+    <meta property="og:url" content="" />
+    <meta property="og:image" content="" />
+    <link rel="stylesheet" href="@staticVersion('/css/davat.css')">
+</head>
+<body>
+    {{-- <div class="bg-brand bg-opacity-20 h-screen flex flex-col items-center justify-center">
+        <button href="#">
+            <img src="{{ asset('/images/gift-sample.png') }}" alt="">
+        </button>
+        <div class="text-gray-600 mt-4 cursor-default">برای کپی کردن کد، روی تصویر کلیک کنید</div>
+    </div> --}}
+    <div class="bg-gray-100 h-screen flex flex-col items-center justify-center">
+        <div class="flex flex-col items-center justify-center text-gray-500 bg-white shadow-md rounded-3xl h-96 w-96">
+            <span>با عرض پوزش</span>
+            <span class="mt-2">.کد تخفیف مورد نظر شما، منقضی شده است</span>
         </div>
-
-        <div class="lg:hidden grid grid-cols-1 xs:grid-cols-2 md:grid-cols-4 gap-4 mt-6">
-            @include('dashboard.gifts.options')
-        </div>
-
-        <div class="grid grid-cols-12 gap-4">
-            <div class="hidden lg:block col-span-1"></div>
-            <div class="col-span-full lg:col-span-10">
-                <div class="mt-8 mb-2">
-                    <h3 class="heading mb-4" data-total="({{ $gift->users->total() }})">کاربران استفاده کننده</h3>
-                    <div class="flex justify-between items-center flex-wrap mb-4">
-                        @include('layouts.quick_search')
-                        @if ($gift->status != 'expires')
-                            <a href="{{ route('dashboard.gifts.appendUserForm', [$gift->region->id, $gift->id]) }}" title="@lang('افزودن کاربر')" class="flex items-center justify-center flex-shrink-0 w-9 sm:w-auto h-9 sm:px-4 text-sm text-green-700 border border-green-700 hover:bg-green-50 hover:bg-opacity-20 rounded-full transition mr-4 focus-current ring-green-700">
-                                <i class="fal fa-plus sm:ml-2"></i>
-                                <span class="hidden sm:inline">@lang('افزودن کاربر')</span>
-                            </a>
-                        @endif
-                    </div>
-                </div>
-                <div>
-                    <div class="hidden sm:flex items-center cursor-default px-2 text-xs variable-font-medium text-white bg-brand py-2 rounded">
-                        <div class="flex-1 px-2">@lang('کاربر')</div>
-                        <div class="flex-1 px-2">@lang('تعداد استفاده')</div>
-                        <div class="flex-1 px-2">@lang('آخرین استفاده')</div>
-                        <div class="flex-1 px-2">@lang('وضعیت')</div>
-                        <div class="w-12 px-2"></div>
-                    </div>
-                    @include('dashboard.gifts.listUsers')
-                </div>
-            </div>
-            <div class="hidden lg:block col-span-1"></div>
-        </div>
+        <a href="https://risloo.ir/" class="en tracking-widest text-gray-400 hover:text-blue-600 transition mt-8">RISLOO.IR</a>
     </div>
-@endsection
+</body>
+</html>
