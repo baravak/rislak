68104ae1bf1a3651176cd7576e2277fbaad586bc
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-12-06 21:42:57 +0330

    Add samples optional 2text answers

---
+++ resources/views/samples/items/answers/optional_2text.blade.php
@@ -0,0 +1,26 @@
+<div class="sample-item-radio-2text">
+    <div class="titles bg-blue-100">هر وقت کسی نیاز به حمایت داشته باشد، احساس می‌کند بیشتر از آن حمایت را دریافت کرده است</div>
+    <div class="items">
+        <div class="item">
+            <input type="radio" name="answers" id="1" value="1" class="sr-only">
+            <label for="1" class="bg-purple-100">1</label>
+        </div>
+        <div class="item">
+            <input type="radio" name="answers" id="2" value="2" class="sr-only">
+            <label for="2" class="bg-purple-50">2</label>
+        </div>
+        <div class="item">
+            <input type="radio" name="answers" id="3" value="3" class="sr-only">
+            <label for="3" class="bg-gray-50">3</label>
+        </div>
+        <div class="item">
+            <input type="radio" name="answers" id="4" value="4" class="sr-only">
+            <label for="4" class="bg-blue-50">4</label>
+        </div>
+        <div class="item">
+            <input type="radio" name="answers" id="5" value="5" class="sr-only">
+            <label for="5" class="bg-blue-100">5</label>
+        </div>
+    </div>
+    <div class="titles bg-purple-100">بعد از اینکه راجع به چیزی صحبت کردیم، هیچ کس متوجه نمی‌شود که باید چه کار کند</div>
+</div>
