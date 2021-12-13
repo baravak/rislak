a164670c3911f6aebf02579b5bb0fefb64c82c53
abouhatef <itb.baravak@gmial.com>
2021-12-13 18:20:33 +0330

    add gift description

--- app/Graphql/Client.php
+++ app/Graphql/Client.php
@@ -13,13 +13,21 @@ class Client{
             'query' => $query,
             'variables' => $variable
         ]);
+        $headers = array_merge(apache_request_headers(), ['authorization' => 'Bearer '. User::token()]);
+        $headers = array_filter($headers, function($value, $key){
+            $deny = ['host', 'content-type', 'content-length'];
+            if(in_array(strtolower($key), $deny)){
+                return false;
+            }
+            return true;
+        },ARRAY_FILTER_USE_BOTH);
         $response = Http::withOptions([
             'verify' => false
         ])
         ->timeout(20)
-        ->withHeaders([
-            'authorization' => 'Bearer '. User::token()
-        ])->withBody($json, 'application/json')->post(env('GRAPH_URL'));
+        ->withHeaders(
+            $headers
+        )->withBody($json, 'application/json')->post(env('GRAPH_URL'));
         $result = $response->object(false);
         if(isset($result->errors) || isset($result->exception)){
             if(isset($result->errors)){
--- resources/views/dashboard/gifts/show.blade.php
+++ resources/views/dashboard/gifts/show.blade.php
@@ -7,10 +7,12 @@
             @include('dashboard.gifts.shareSide')
             <div class="col-span-full sm:col-span-7 lg:col-span-5 flex flex-col">
                 <div class="bg-gray-100 bg-opacity-50 border border-gray-200 rounded-lg p-4 relative">
-                    <div class="text-left absolute left-3 right-3 top-3 cursor-default" x-data="alpinePopper()">
-                        <i class="fal fa-info-circle text-lg text-gray-500" x-ref="trigger"></i>
-                        <span class="block variable-font-light bg-gray-700 text-white rounded-md p-2 text-sm text-right" x-ref="popper">صوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرا</span>
-                    </div>
+                    @if ($gift->description)
+                        <div class="text-left absolute left-3 right-3 top-3 cursor-default" x-data="alpinePopper()">
+                            <i class="fal fa-info-circle text-lg text-gray-500" x-ref="trigger"></i>
+                            <span class="block variable-font-light bg-gray-700 text-white rounded-md p-2 text-sm text-right" x-ref="popper">{{ $gift->description }}</span>
+                        </div>
+                    @endif
                     <div class="grid grid-cols-2 gap-4 cursor-default pt-6 xs:pt-0">
                         <div>
                             <span class="block text-xs text-gray-500">تاریخ شروع اعتبار</span>
