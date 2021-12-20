74fe35d4733bf049aa8f601ad03f7dcfb8d7fbb2
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-12-20 13:41:14 +0330

    Update center & room accounting assessments

--- resources/views/dashboard/centers/accounting/assessments/list.blade.php
+++ resources/views/dashboard/centers/accounting/assessments/list.blade.php
@@ -1,8 +1,13 @@
 <div data-xhr="gift-items">
     <div class="hidden sm:flex items-center cursor-default px-2 text-xs variable-font-medium text-gray-600 bg-gray-100 py-2 rounded">
-        <div class="w-40 pl-2">@lang('Serial')</div>
+        <div class="w-40 pl-2 hidden lg:block">@lang('Serial')</div>
         <div class="flex-1 px-2">@lang('Scale')</div>
-        <div class="flex-1 px-2">@lang('Amount')</div>
+        {{-- @if ($room) --}}
+            {{-- <div class="flex-1 px-2">@lang('مبلغ در مرکز')</div>
+            <div class="flex-1 px-2">@lang('مبلغ در اتاق')</div> --}}
+        {{-- @else --}}
+            <div class="flex-1 px-2">@lang('Amount')</div>
+        {{-- @endif --}}
     </div>
     {{-- @foreach ($assessments as $assessment) --}}
         @include('dashboard.centers.accounting.assessments.listRaw')
--- resources/views/dashboard/centers/accounting/assessments/listRaw.blade.php
+++ resources/views/dashboard/centers/accounting/assessments/listRaw.blade.php
@@ -1,9 +1,10 @@
 <div class="flex flex-col sm:flex-row sm:items-center bg-gray-50 hover:bg-gray-100 transition py-3 pb-4 sm:py-2 p-2 rounded mt-2">
-    <div class="w-40 items-center cursor-default pl-2">
+    <div class="w-40 cursor-default pl-2 hidden lg:flex">
         <span class="block text-xs text-gray-600 en dir-ltr text-right">$OtherCustom-9A</span>
     </div>
     <div class="flex-1 px-2 cursor-default">
         <div class="text-xs text-gray-600 variable-font-medium lg:variable-font-normal">آزمون اختصاصی آزمون‌های گروهی پرسش‌نامه پایان‌نامه‌ای</div>
+        <span class="block text-xs text-gray-600 en dir-ltr text-right mt-1 lg:hidden">$OtherCustom-9A</span>
         <div class="flex items-center text-xs text-gray-400 mt-1">
             {{-- @if ($edition) --}}
                 <span>@lang('Edition') طلیعه سلامت</span>
@@ -14,10 +15,29 @@
             {{-- @endif --}}
         </div>
     </div>
-    <div class="flex-1 px-2">
-        <div class="flex items-center">
-            <input type="number" value="0" class="text-left dir-ltr w-24 h-7 border border-gray-300 rounded text-sm text-gray-600 focus">
-            <span class="text-xs text-gray-600 mr-2 pt-0.5">90٬000 @lang('تومانءءء')</span>
+    {{-- @if ($room) --}}
+        {{-- <div class="flex-1 px-2 mt-2 sm:mt-0 cursor-default">
+            <span class="text-xs text-gray-600 sm:hidden ml-2">@lang('مبلغ در مرکز'):</span>
+            <span class="text-xs text-gray-600 pt-0.5 variable-font-medium sm:variable-font-normal">90٬000 @lang('تومانءءء')</span>
         </div>
-    </div>
+        <div class="flex-1 px-2 mt-1 sm:mt-0">
+            <label for="default" class="flex items-center cursor-pointer group">
+                <span class="text-xs text-gray-600 sm:hidden cursor-default ml-2">@lang('مبلغ در اتاق'): </span>
+                <input id="default" type="checkbox" class="w-4 h-4 rounded border border-gray-400 focus">
+                <span class="text-xs text-gray-600 mr-1.5 group-hover:text-blue-600">@lang('پیش‌فرض مرکز')</span>
+            </label>
+            <div class="flex items-center mt-2">
+                <input type="number" value="0" class="text-left dir-ltr w-28 h-7 border border-gray-300 rounded text-xs text-gray-600 focus">
+                <span class="text-xs text-gray-600 mr-2 pt-0.5">90٬000 @lang('تومانءءء')</span>
+            </div>
+        </div> --}}
+    {{-- @else --}}
+        <div class="flex-1 px-2 mt-2 sm:mt-0">
+            <span class="text-xs text-gray-600 sm:hidden ml-2">@lang('Amount'):</span>
+            <div class="flex items-center mt-1 sm:mt-0">
+                <input type="number" value="0" class="text-left dir-ltr w-28 h-7 border border-gray-300 rounded text-xs text-gray-600 focus">
+                <span class="text-xs text-gray-600 mr-2 pt-0.5">90٬000 @lang('تومانءءء')</span>
+            </div>
+        </div>
+    {{-- @endif --}}
 </div>
