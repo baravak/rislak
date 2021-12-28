7b6b81a2d953e9f5b7f5e0b4491377bf3cdfc8b4
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-12-28 14:56:26 +0330

    Update sample matrix

--- resources/views/samples/items/answers/matrix.blade.php
+++ resources/views/samples/items/answers/matrix.blade.php
@@ -1,42 +1,46 @@
-<div class="sample-matrix">
-    <div>
-        <h3>@lang('مادر')</h3>
-        <div class="items hidden sm:grid">
-            <div class="item">
-                <input type="radio" name="matrix-item" id="matrix-item-1" class="sr-only">
-                <label for="matrix-item-1" class="bg-red-400 bg-opacity-30">@lang('کاملا غلط')</label>
-            </div>
-            <div class="item">
-                <input type="radio" name="matrix-item" id="matrix-item-2" class="sr-only">
-                <label for="matrix-item-2" class="bg-red-400 bg-opacity-20">@lang('تقریبا غلط')</label>
-            </div>
-            <div class="item">
-                <input type="radio" name="matrix-item" id="matrix-item-3" class="sr-only">
-                <label for="matrix-item-3" class="bg-green-400 bg-opacity-20">@lang('بیشتر درست است تا غلط')</label>
-            </div>
-            <div class="item">
-                <input type="radio" name="matrix-item" id="matrix-item-4" class="sr-only">
-                <label for="matrix-item-4" class="bg-green-400 bg-opacity-30">@lang('اندکی درست')</label>
-            </div>
-            <div class="item">
-                <input type="radio" name="matrix-item" id="matrix-item-5" class="sr-only">
-                <label for="matrix-item-5" class="bg-green-400 bg-opacity-40">@lang('تقریبا درست')</label>
-            </div>
-            <div class="item">
-                <input type="radio" name="matrix-item" id="matrix-item-6" class="sr-only">
-                <label for="matrix-item-6" class="bg-green-400 bg-opacity-50">@lang('کاملا درست')</label>
-            </div>
+<div>
+    <div class="hidden md:block sample-matrix">
+        <div class="matrix-grid">
+            <div></div>
+            <h3>@lang('کاملا غلط')</h3>
+            <h3>@lang('تقریبا غلط')</h3>
+            <h3>@lang('بیشتر درست است تا غلط')</h3>
+            <h3>@lang('اندکی درست')</h3>
+            <h3>@lang('تقریبا درست')</h3>
+            <h3>@lang('کاملا درست')</h3>
         </div>
-        <div class="sm:hidden mt-2">
-            <select name="" id="">
-                <option value="" disabled selected>@lang('انتخاب کنید')</option>
-                <option value="">@lang('کاملا غلط')</option>
-                <option value="">@lang('تقریبا غلط')</option>
-                <option value="">@lang('بیشتر درست است تا غلط')</option>
-                <option value="">@lang('اندکی درست')</option>
-                <option value="">@lang('تقریبا درست')</option>
-                <option value="">@lang('کاملا درست')</option>
-            </select>
+        <div class="matrix-grid">
+            <h4 class="bg-gray-50 {{-- bg-gray-100 --}}">@lang('پدر')</h4>
+            <label class="bg-gray-50 {{-- bg-gray-100 --}}">
+                <input type="radio" name="matrix-1" id="matrix-1-1">
+            </label>
+            <label class="bg-gray-50 {{-- bg-gray-100 --}}">
+                <input type="radio" name="matrix-1" id="matrix-1-2">
+            </label>
+            <label class="bg-gray-50 {{-- bg-gray-100 --}}">
+                <input type="radio" name="matrix-1" id="matrix-1-3">
+            </label>
+            <label class="bg-gray-50 {{-- bg-gray-100 --}}">
+                <input type="radio" name="matrix-1" id="matrix-1-4">
+            </label>
+            <label class="bg-gray-50 {{-- bg-gray-100 --}}">
+                <input type="radio" name="matrix-1" id="matrix-1-5">
+            </label>
+            <label class="bg-gray-50 {{-- bg-gray-100 --}}">
+                <input type="radio" name="matrix-1" id="matrix-1-6">
+            </label>
         </div>
+    </div>
+    <div class="md:hidden mt-2 sample-matrix-select">
+        <h4>@lang('پدر')</h4>
+        <select name="" id="">
+            <option value="" disabled selected>@lang('انتخاب کنید')</option>
+            <option value="">@lang('کاملا غلط')</option>
+            <option value="">@lang('تقریبا غلط')</option>
+            <option value="">@lang('بیشتر درست است تا غلط')</option>
+            <option value="">@lang('اندکی درست')</option>
+            <option value="">@lang('تقریبا درست')</option>
+            <option value="">@lang('کاملا درست')</option>
+        </select>
     </div>
 </div>
