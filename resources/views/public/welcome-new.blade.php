aec7c8fd374fbdd858e31aa46ac1d368330af963
Abouhatef <itb.baravak@gmail.com>
2023-06-18 17:50:44 +0330

    Update landing page

--- resources/views/public/welcome-new.blade.php
+++ resources/views/public/welcome-new.blade.php
@@ -384,8 +384,12 @@
                             </div>
                         </div>
                         <div class="flex flex-shrink-0">
-                            <a class="w-20" referrerpolicy="origin" target="_blank" href="https://trustseal.enamad.ir/?id=223057&amp;Code=nI17RKpP7XMHZTzmer28" title="نماد اعتماد الکترونیکی ریسلو" aria-label="نماد اعتماد الکترونیکی ریسلو">
+                            {{-- <a class="w-20" referrerpolicy="origin" target="_blank" href="https://trustseal.enamad.ir/?id=223057&amp;Code=nI17RKpP7XMHZTzmer28" title="نماد اعتماد الکترونیکی ریسلو" aria-label="نماد اعتماد الکترونیکی ریسلو">
                                 <img referrerpolicy="origin" src="{{ asset('/images/logo/enamad-new-logo.png') }}" alt="" style="cursor:pointer;" id="nI17RKpP7XMHZTzmer28">
+                            </a> --}}
+
+                            <a class="w-20"  referrerpolicy="origin" target="_blank" href="https://trustseal.enamad.ir/?id=223057&amp;Code=nI17RKpP7XMHZTzmer28">
+                                <img referrerpolicy="origin" src="https://Trustseal.eNamad.ir/logo.aspx?id=223057&amp;Code=nI17RKpP7XMHZTzmer28" alt="" style="cursor:pointer" id="nI17RKpP7XMHZTzmer28">
                             </a>
                         </div>
                     {{-- </div> --}}
