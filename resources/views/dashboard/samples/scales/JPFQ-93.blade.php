93795be32bc4d76bd2b329597a4c432e8e328723
abouhatef <itb.baravak@gmail.com>
2024-01-09 15:07:22 +0330

    JPFQ93 added profile

---
+++ resources/views/dashboard/samples/scales/JPFQ-93.blade.php
@@ -0,0 +1,18 @@
+@php
+    $_aProfiles = [
+        ['نیم‌رخ پژوهشی', 'profile_items_sort_key_png', 'profile_page2_png'],
+];
+@endphp
+<div data-xhr="sample-profiles" id="sample-profile">
+    <h3 class="heading">پاسخ‌ها</h3>
+    <div class="mt-4">
+        @foreach ($_aProfiles as $item)
+            @if ($scoring->profiles && ($prf = $scoring->profiles->whereIn('mode', [$item[1], $item[2]])->first()))
+                <a href="{{ $prf->url }}" target="_blank" class="inline-block magnific-popup ml-4">
+                    <img src="{{ $prf->url }}" class="w-32 h-32 object-cover border border-gray-200 p-1 rounded">
+                    <span class="block text-xs text-gray-400 mt-2 text-center">{{ $item[0] }}</span>
+                </a>
+            @endif
+        @endforeach
+    </div>
+</div>
