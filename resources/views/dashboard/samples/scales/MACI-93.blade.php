4a851ab51830da0359d61324c3ca0bce6bd331e3
abouhatef <itb.baravak@gmail.com>
2024-07-11 12:54:43 +0330

    added MACIA93

---
+++ resources/views/dashboard/samples/scales/MACI-93.blade.php
@@ -0,0 +1,19 @@
+@php
+    $_aProfiles = [
+        ['صفحه دوم', 'profile_items_sort_key_png', 'profile_page2_png'],
+        ['صفحه سوم', 'profile_items_sort_key_png', 'profile_page3_png'],
+        ['صفحه چهارم', 'profile_items_sort_key_png', 'profile_page4_png'],
+];
+@endphp
+<div data-xhr="sample-profiles" id="sample-profile">
+    <div class="mt-4">
+        @foreach ($_aProfiles as $item)
+            @if ($scoring->profiles && ($prf = $scoring->profiles->whereIn('mode', [$item[1], $item[2]])->first()))
+                <a href="{{ $prf->url }}" target="_blank" class="inline-block magnific-popup ml-4">
+                    <img src="{{ $prf->url }}" class="w-32 h-32 object-cover border border-gray-200 p-1 rounded">
+                    <span class="block text-xs text-gray-400 mt-2 text-center">{{ $item[0] }}</span>
+                </a>
+            @endif
+        @endforeach
+    </div>
+</div>
