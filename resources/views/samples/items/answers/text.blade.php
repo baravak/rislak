1c86325f959ff110878e32c8507f882bc09bf3da
abouhatef <itb.baravak@gmail.com>
2025-08-29 10:35:26 +0330

    bugfix

--- resources/views/samples/items/answers/text.blade.php
+++ resources/views/samples/items/answers/text.blade.php
@@ -1,3 +1,8 @@
 <div>
-    <input type="text" step="1" min="0" max="200" name="item-{{ $key + 1 }}" id="item-{{ $key+1 }}"class="w-full text-sm text-gray-600 border border-gray-300 rounded resize-none placeholder-gray-300 p-4" data-merge='[{{ $key+1 }}]' value="{{ isset($item->user_answered) ? $item->user_answered : '' }}">
+<textarea name="item-{{ $key + 1 }}" id="item-{{ $key+1 }}"
+    placeholder="پاسخ را در این قسمت بنویسید"
+    class="w-full text-sm text-gray-600 border border-gray-300 rounded resize-none placeholder-gray-300 p-4"
+    data-merge='[{{ $key+1 }}]'
+    data-item-value
+    >{{ isset($item->user_answered) ? $item->user_answered : '' }}</textarea>
 </div>
